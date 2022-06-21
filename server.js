4188e852124ebdc48862adf1ccc2ff233508b064
abdulrahman atef <abdulrahman.atef@iken.tech>
2022-06-21 17:33:42 +0200

    fix server issues

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
-const workoutRouter = require("./routes/workouts");
+const workoutRoutes = require("./routes/workouts");
 
 // create express app
 const app = express();
 
 // middlewares
-app.use(express.json);
+app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(req.method, req.path);
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 // add routes
-app.use("api/workouts", workoutRouter);
+app.use("/api/workouts", workoutRoutes);
 
 // listen on port
 app.listen(process.env.PORT, () => {
