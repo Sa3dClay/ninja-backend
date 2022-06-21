8349db037f58efc51c1f5650610fd29925a0cc06
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-06-21 23:55:15 +0200

    connect mongo db with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 // create express app
@@ -17,7 +18,15 @@ app.use((req, res, next) => {
 // add routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen on port
-app.listen(process.env.PORT, () => {
-    console.log("listening on port", process.env.PORT);
-});
+// connect db
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen on port
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db & listening on port", process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
