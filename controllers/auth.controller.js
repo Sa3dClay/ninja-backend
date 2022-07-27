ea02ce58298291722d173fa73f0909e6e691f1d1
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-07-27 22:49:20 +0200

    signup new users with unique email install bcrypt to hash passwords

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const userModel = require("../models/user.model");
 
 // login
@@ -8,7 +7,15 @@ const login = async (req, res) => {
 
 // signup
 const signup = async (req, res) => {
-    res.json({ msg: "signup user" });
+    const { email, password } = req.body;
+
+    try {
+        const user = await userModel.signup(email, password);
+
+        res.status(200).json({ email, user });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 module.exports = { login, signup };
--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
     {
@@ -17,5 +18,17 @@ const userSchema = new Schema(
         timestamps: true,
     }
 );
+
+userSchema.statics.signup = async function(email, password) {
+    const exists = await this.findOne({ email });
+
+    if (exists) throw Error("Email already exists");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await this.create({ email, password: hashedPassword });
+
+    return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
